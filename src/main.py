2ad3c4b4ffed3b8e38a618854c624e32afa84b72
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-08-26 01:52:17 -0400

    structured code, design almost finalized

--- src/main.py
+++ src/main.py
@@ -4,19 +4,76 @@ import websockets
 import json
 from json.decoder import JSONDecodeError
 
+from round_robin import get_schedule
+
 class Game:
     def __init__(self, player1, player2):
-        pass
+        self.player1_key  = player1
+        self.player2_key  = player2
+        self.player1_hand = []
+        self.player2_hand = []
+        self.discard_pile = []
+        self.draw_pile    = []
 
     async def play(self):
         pass
 
+    def get_keys(self):
+        return (self.player1_key, self.player2_key)
+
 """
-WEBSOCKETS SERVER LOGIC
+CURRENT GAMES
+"""
+#player_key : Game
+GAMES = dict()
+
+def add_game(game):
+    global GAMES
+    p1, p2 = game.get_keys() 
+    GAMES[p1] = game
+    GAMES[p2] = game
+
+def remove_game(game):
+    global GAMES
+    p1, p2 = game.get_keys() 
+    del GAMES[p1]
+    del GAMES[p2]
+
+def get_game(key):
+    if key in GAMES:
+        return GAMES[key]
+    return None
+
+"""
+PERMISSIBLE KEYS
 """
 KEYS = set()
 
-REGISTERED = dict() #player_key : websocket
+def parse_keys():
+    global KEYS
+    with open('keys.txt', 'r') as f:
+        for line in f.readlines():
+            KEYS.add(line.rstrip('\n'))
+
+"""
+REGISTERED KEYS
+"""
+REGISTERED = set()
+
+def register_player(key):
+    global REGISTERED
+    REGISTERED.add(key)
+
+def unregister_player(key):
+    global REGISTERED
+    REGISTERED.remove(key)
+
+def is_registered(key):
+    return key in REGISTERED
+
+"""
+WEBSOCKETS SERVER LOGIC
+"""
 
 def is_valid_init_message(msg):
     return 'type' in msg and msg['type'] == 'init' and 'key' in msg
@@ -25,22 +82,12 @@ def has_valid_key(msg):
     key = msg['key']
     return key in KEYS and not key in REGISTERED
 
-def register_player(websocket, key):
-    global REGISTERED
-    REGISTERED[key] = websocket
-
-def unregister_player(key):
-    global REGISTERED
-    del REGISTERED[key]
-
-def is_registered(key):
-    return key in REGISTERED
-
 #handles any subsequent messages (and publishes messages as well)
 async def general_handler(key, websocket):
     await asyncio.Future()
 
 #handles the parsing of init message upon connection
+#add a refusal after the signup time has expired
 async def init_handler(websocket):
     key = None
     try:
@@ -53,7 +100,7 @@ async def init_handler(websocket):
 
             #register player
             print(f"registered player {key}")
-            register_player(websocket, key)
+            register_player(key)
 
             #handle further messages from this socket
             await general_handler(key, websocket)
@@ -64,7 +111,7 @@ async def init_handler(websocket):
         await websocket.close(code=1003, reason=str(e))
 
     except websockets.ConnectionClosedOK:
-        if not (key is None) and is_registered(key):
+        if not key is None and is_registered(key):
             unregister_player(key)
 
 #starts websockets server
@@ -75,14 +122,38 @@ async def main():
 """
 MATCHMAKING AND GAME SIMULATION LOGIC
 """
-async def match_make():
-    await asyncio.Future()
+async def play_game(key_pair):
+    player1, player2 = key_pair
+    game = Game(player1, player2)
 
+    add_game(game)
+    await game.play()
+    remove_game(game)
+
+async def match_make():
+    #sleep until tournament start
+    await asyncio.sleep(5)
+
+    #make round robin schedule
+    schedule = get_schedule(REGISTERED)
+
+    #iterate through round robin rounds
+    for round in schedule:
+        coroutines = [play_game(pair) for pair in round]
+
+        #games played in parallel in each round
+        game_loop = asyncio.get_event_loop()
+        game_loop.run_until_complete(asyncio.gather(*coroutines))
+        game_loop.close()
+
+    print("All games played. Ending...")
+
+"""
+MAIN SCRIPT
+"""
 if __name__ == '__main__':
     #parse keys
-    with open('keys.txt', 'r') as f:
-        for line in f.readlines():
-            KEYS.add(line.rstrip('\n'))
+    parse_keys()
     print(KEYS)
 
     #run websockets server
---
+++ src/round_robin.py
@@ -0,0 +1,25 @@
+def shift(lst):
+    last  = lst[-1]
+    lst[1:] = lst[:-1]
+    lst[0] = last
+    return lst
+
+def get_round(keys):
+    pair_amount = len(keys) // 2
+    round = [(keys[i], keys[i+pair_amount]) for i in range(pair_amount)]
+    return round
+
+def get_schedule(registered):
+    keys = list(registered)
+    if len(keys) % 2 == 1:
+        keys.append(None) #add dummy key if uneven
+
+    schedule = []
+
+    last_keys  = keys
+    schedule.append(get_round(keys))
+    for round in range(len(keys) - 2):
+        last_keys[1:] = shift(last_keys[1:])
+        schedule.append(get_round(keys))
+    
+    return schedule
