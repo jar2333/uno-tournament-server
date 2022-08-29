ff65b84d952d00c5c7dab4bd00661a17b5c1ed4e
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-08-29 02:58:42 -0400

    added typing to game factory method

--- src/game_factory.py
+++ src/game_factory.py
@@ -3,5 +3,5 @@ from game import Game
 
 from uno_game import UnoGame
 
-def get_new_game(player1: str, player2: str):
+def get_new_game(player1: str, player2: str) -> Game:
     return UnoGame(player1, player2)
