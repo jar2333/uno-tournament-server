66c54ca0dababe9b7a2e5ac317716bde02be4d83
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-09-01 08:06:15 -0400

    implemented one command

--- src/uno_game.py
+++ src/uno_game.py
@@ -2,6 +2,10 @@ from game import Game
 
 from random import shuffle
 
+COLORS = ["red", "blue", "yellow", "green"]
+
+ONE_MORE = ["skip", "reverse", "+2", "+4"]
+
 class UnoGame(Game):
     def __init__(self, p1: str, p2: str):
         super().__init__(p1, p2)
@@ -18,13 +22,55 @@ class UnoGame(Game):
 
         self.discard_pile = []
 
+        #a bunch of flags used for implementing game rules
+
     def interpret_message(self, key: str, message: dict):
+        hand = self.hands[key]
+        opponent_key = self.get_opponent_key(key)
         match message:
             case {'type': 'draw'}:
                 pass
 
-            case {'type': 'play', 'index': i}:
+            case {'type': 'play', 'index': index}:
                 pass
+
+            case {'type': 'play', 'index': index, "color": chosen_color}:
+                try:
+                    i = int(index)
+                    card = hand[i]
+
+                    if not self.__is_valid(card):
+                        return None
+
+                    number, color = card
+
+                    if color == "colorless":
+                        if chosen_color in COLORS:
+                            self.discard_pile.append((number, chosen_color))
+                        else:
+                            return None
+                    else:
+                        self.discard_pile.append(card)
+
+                    self.hands[key].pop(i)
+
+                    if number in ["skip", "reverse", "+2", "+4"]:
+                        match number:
+                            case "+2":
+                                for i in range(2):
+                                    self.__draw_card(opponent_key)
+                                return False
+                            case "+4":
+                                for i in range(4):
+                                    self.__draw_card(opponent_key)
+                                return False
+                            case _:
+                                return False
+
+                    return True
+
+                except:
+                    return None
 
             case {'type': 'challenge'}:
                 pass
@@ -33,20 +79,46 @@ class UnoGame(Game):
                 pass
 
             case _:
-                pass
-
-        return None
+                return None
 
     def get_state(self, key: str):
-        return {}
+        if len(self.discard_pile) == 0:
+            d = None
+        else:
+            d = self.discard_pile[-1]
+
+        opponent_key = self.get_opponent_key(key)
+        opponent_hand_size = len(self.hands[opponent_key])
+
+        return {"player_hand": self.hands[key], "discard": d, "opponent_hand_size": opponent_hand_size}
+
+    def __is_valid(self, card):
+        number, color = card
+        
+        discard_number, discard_color = self.discard_pile[-1]
+
+        if number == discard_number or color == discard_color or number == "wild":
+            return True
+
+        return False
+
+    # def __evaluate_action(self, key, card):
+    #     #evaluate effect of action cards
+    #     pass
+
+    def __draw_card(key):
+        #draw 1 from draw_pile and cycle discard if necessary
+        pass
+
 
 def make_deck():
     cards = []
     for number in [str(x) for x in range(10)] + ["skip", "reverse", "+2", "wild", "+4"]:
         if number in ["wild", "+4"]:
-            cards.append((number, None))
+            for i in range(4):
+                cards.append((number, "colorless"))
         else:
-            for color in ["red", "blue", "yellow", "green"]:
+            for color in COLORS:
                 amount = 2
                 if number == "0":
                     amount = 1
