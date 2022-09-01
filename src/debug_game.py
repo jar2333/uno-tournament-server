8c922e26b5aaeef5c22733864207a404bc63898c
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-09-01 08:17:21 -0400

    made debug game

--- src/debug_game.py
+++ src/debug_game.py
@@ -1,21 +1,14 @@
 from game import Game
 
-def is_valid_debug_message(message: dict) -> bool:
-    if not 'type' in message:
-        return False
-
-    #debug
-    if message['type'] != 'debug':
-        return False
-
-    return True
-
 class DebugGame(Game):
     def __init__(self, p1: str, p2: str):
         super().__init__(p1, p2)
 
     def interpret_message(self, key: str, message: dict):
-        return is_valid_debug_message(message)
+        if 'type' in message and message['type'] == 'debug':
+            return True
+            
+        return None
 
     def get_state(self, key: str) -> dict:
         return {"turn": self.player1_key}
